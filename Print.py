d75cd28aea81d971f05ac04ff7b87cadba706553
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-02 11:35:44 +0530

    first commit

---
+++ Print.py
@@ -0,0 +1,20 @@
+# Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+# It is used for:
+
+# web development (server-side),
+# software development,
+# mathematics,
+# system scripting.
+
+# Python is a popular, high-level programming language known for its simplicity and readability.
+
+# Python’s syntax is designed to be easy to understand and write, making it a great choice for beginners. 
+
+
+print("Hello Python")
+print("Namstey",1,7.7,True)
+print("Namstey",1,7.7,True,sep='-')
+
+print("Namstey", end='-')
+print ("Bharat")
