9bde19c0cec0c4938225845a99bfa569d8acd550
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-02 12:00:08 +0530

    update

--- Variables.py
+++ Variables.py
@@ -1,2 +1,18 @@
 name='avneesh'
 print(name)
+a=3
+b=5
+print(a+b)
+
+# Dynamic Typing
+a=5
+
+#static Typing
+# int a=5
+
+# Dynamic binding vS Static binding
+a=5
+a="avneesh"
+
+a,b,c=1,2,3
+print(a,b,c)
