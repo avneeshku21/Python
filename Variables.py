07918985a10d467e1ba0f1daff31044fddf10fcb
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-02 12:45:17 +0530

    update

--- Variables.py
+++ Variables.py
@@ -16,3 +16,18 @@ a="avneesh"
 
 a,b,c=1,2,3
 print(a,b,c)
+
+#literals
+# jo ham varibale m value store krte hai usko Literals Khte hai
+a= 0b1010 # Binary
+b=100 #Decimal
+c=0o310 # Ocatal
+d=0x12c # HexaDecimal
+
+# Float
+float_1=10.5
+float_2=1.5e2
+float_3=1.5e-3
+
+# Complex Literals
+x=3.14j
