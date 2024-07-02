06390d755ce315cd7ac5c9c447f3b69acff3e146
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-02 11:48:56 +0530

    Update

---
+++ DataType.py
@@ -0,0 +1,43 @@
+# Intergers
+# Decimal
+# Boolean
+#Text/ Strings
+#Complex
+#List -- Array
+#Tuples
+# Sets
+#Dictionary
+
+
+#************************************
+
+# Intergers
+
+print(8)
+# print(1e308)
+
+
+# Decimal
+print(8.55)
+# print(1.7e309)
+
+# Boolean
+print (True)
+
+#Text/ Strings
+print("Hello")
+
+#Complex
+print(5+6j)
+
+#List -- Array
+print([1,2,3,4,5])
+
+#Tuples
+print((1,2,3,4,6))
+
+# Sets
+print({1,2,3,4,5})
+
+#Dictionary
+print({'name':'AVneesh','gender':'Male',})
