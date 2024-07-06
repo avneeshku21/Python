467df93302497abc61d9ed52b9b055c14847dfbf
Avneesh Kumar <kavneesh9045@gmail.com>
2024-07-06 17:29:43 +0530

    Update ifElse.py

--- Session2/ifElse.py
+++ Session2/ifElse.py
@@ -10,17 +10,22 @@
 # elif a == b:
 #   print("a and b are equal")
 
-a = 500
-b=110
-c=100
-if a > b and c > a:
-  print("Both conditions are True")
+# a = 500
+# b=110
+# c=100
+# if a > b and c > a:
+#   print("Both conditions are True")
 
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
