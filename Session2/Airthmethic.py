df7eb14b5dcc07d1e69910223698da7d37bdbb8e
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-06 16:57:41 +0530

    update

--- Session2/Airthmethic.py
+++ Session2/Airthmethic.py
@@ -6,3 +6,26 @@ print(5/2)# Normal Division
 print(5//2) # integer Divison
 print(5%2)# it give Rem
 print(5**2)# power
+
+# ***************Relational Opertators
+
+# Equal To(==), 
+# Not Equal To(! =), 
+# Greater Than(>), 
+# Less Than(<), 
+# Greater Than or Equal To(>=),
+# Lesser Than or Equal To(<=)
+
+#**************Logical Operators 
+print(1 and 0) # dono 1 honge to  true
+print (1 or 0) # dono me se ek bhi hoga to true
+print(not 1)
+
+
+#***************bitwise operator
+
+print(2&3)
+
+print (2|3)
+
+
