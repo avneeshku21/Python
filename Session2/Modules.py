a6dc9077fde2e2b3c4dd94b0eaaba41cef2b36d8
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-06 17:53:06 +0530

    update

---
+++ Session2/Modules.py
@@ -0,0 +1,21 @@
+#modules in pyThon
+
+# Math 
+#keyWord
+#Random
+#datetime
+
+
+# import math
+# print(math.factorial(3))
+
+# import keyword
+# print(keyword.kwlist)
+
+# import random
+# print(random.randint(1,100))
+
+import datetime
+print(datetime.datetime.now())
+
+help('modules')
---
+++ Session2/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+import math
+# print(math.factorial(3))
+
+# import keyword
+# print(keyword.kwlist)
+
+# import random
+# print(random.randint(1,100))
