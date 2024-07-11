96461e5b50a158047f0a695bc60fbc916946f4ac
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-11 11:39:39 +0530

    update

--- Session3/Question2.py
+++ Session3/Question2.py
@@ -1,15 +1,23 @@
-rows = int(input("Enter number of rows: "))
+# rows = int(input("Enter number of rows: "))
 
-for i in range(1,rows):
-    for j in range(1,i+1):
-        print("* ", end="")
-    print() # for new line
+# for i in range(1,rows):
+#     for j in range(1,i+1):
+#         print("* ", end="")
+#     print() # for new line
 
 
 
-row = int(input("Enter number of rows: "))
+# row = int(input("Enter number of rows: "))
 
-for i in range(row):
-    for j in range(i+1):
-        print(j+1, end="")
-    print()
+# for i in range(1,row):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+rows = int(input('enter number of rows: '))
+for i in range(1,rows+1):
+  for j in range(1,i+1):
+    print(j,end='')
+  for k in range(i-1,0,-1):
+    print(k,end='')
+  print()
---
+++ Session3/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+row = int(input("Enter number of rows: "))
+
+# for i in range(1,row):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
