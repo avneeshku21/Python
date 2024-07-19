5006238d3c857f88e5189b1db76323780d71a484
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-19 14:58:43 +0530

    update

---
+++ ChaiAurCodePy/List/List.py
@@ -0,0 +1,11 @@
+# tea_varites=["Black", "Green Tea", " Oolong","white"]
+# print(tea_varites[0])
+
+# thislist = ["apple", "banana", "cherry"]
+# print(len(thislist))
+
+# mylist = ["apple", "banana", "cherry"]
+# print(type(mylist))
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
