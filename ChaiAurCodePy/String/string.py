f9bd723c71916233c23f01990ee7c9dcf1e9a34c
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-19 14:52:44 +0530

    update

---
+++ ChaiAurCodePy/String/string.py
@@ -0,0 +1,25 @@
+# chai="Lemon Chai"
+# chai ="masasla chai"
+# print(chai)
+
+# slice_chai =chai[0:6]
+# print(slice_chai)
+
+# b = "Hello, World!"
+# print(b[2:5])
+
+# a = ("a", "b", "c", "d", "e", "f", "g", "h")
+# x = slice(0, 8, 3)
+# print(a[x])
+
+# chai="     masalaCHai      "
+
+# print(chai.strip())
+
+# chai="leamon, ginger, Masala, Mint"
+# print(chai.split(', '))
+
+# chai_type="Adrak"
+# quant=2
+# order=" I orderd {} cups of{} chai"
+# print(order.format(quant,chai_type))
