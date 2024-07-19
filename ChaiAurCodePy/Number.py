52125003b8f27346996202c15c3fbef9a69f0ea0
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-19 12:06:55 +0530

    update

--- ChaiAurCodePy/Number.py
+++ ChaiAurCodePy/Number.py
@@ -7,3 +7,12 @@
 # a=40.23
 # b=23
 # print(int(a))
+
+# These methods are .__repr__() and .__str__().
+
+# The .__repr__() method returns a detailed description for a programmer who needs to maintain and debug the code. 
+# The .__str__() method returns a simpler description with information for the user of the program.
+
+# .__repr__() provides the official string representation of an object, aimed at the programmer.
+ 
+# .__str__() provides the informal string representation of an object, aimed at the user.
