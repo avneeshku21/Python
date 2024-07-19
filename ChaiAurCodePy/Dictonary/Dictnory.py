0a943ac6f194f8044f7026af23008fb4dfb88d3e
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-19 17:20:30 +0530

    Update

---
+++ ChaiAurCodePy/Dictonary/Dictnory.py
@@ -0,0 +1,49 @@
+chai_type={
+    "Masla":"spicy",
+    "Ginger":"Zesty",
+    "Green":"Mild"
+}
+
+chai_type["Green"]="Fresh"
+
+# for chai in chai_type:
+#     print(chai,chai_type[chai])
+
+# basic syntax
+# for key, value in chai_type.items():
+#     print(key, value)
+
+# if "Masla" in chai_type:
+#     print("Present")
+
+# add new one element
+chai_type["Earl Grey"]="Citrus"
+# print(chai_type)
+
+#*****pop method
+# chai_type.popitem()
+# chai_type.pop("Ginger")
+# del chai_types["Green"]
+
+#******Copy
+# chai_type_copy=chai_type.copy()
+# print(chai_type_copy)
+
+
+#Sytex {{},{}}
+tea_shop={
+    "chai":{"Masala":"Spicy"," ginger":"Zesty"},
+    "Tea":{"Green":"Mild","Black":"Strong"}
+}
+
+# print(tea_shop)
+
+
+Squared_num={x:x**2 for x in range(6)}
+print(Squared_num)
+
+# delat All
+#Squared_num.clear()
+
+
+
