ed516dcd95eebda05246c18b5b5cf64ac08217f7
Avneesh Kumar <kavneesh9045@gmail.com>
2024-07-20 21:56:15 +0530

    Update Dictnory.py

--- ChaiAurCodePy/Dictonary/Dictnory.py
+++ ChaiAurCodePy/Dictonary/Dictnory.py
@@ -1,3 +1,8 @@
+# Dictionaries are used to store data values in key:value pairs.
+
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+
 chai_type={
     "Masla":"spicy",
     "Ginger":"Zesty",
