049e985c36b330c3dee8f4fb822b515bb648531a
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-21 11:42:01 +0530

    update

--- ChaiAurCodePy/Tuples/Tuples.py
+++ ChaiAurCodePy/Tuples/Tuples.py
@@ -2,4 +2,9 @@
 ## why use Tuples- Because it is immutable  ye Change nhi hota
 #tuple object  doses not support item assignment
 tea_types=("black","Green","Oolong")
-
+print(len(tea_types))
+more_tea=("HErbal", "Ear Grey")
+alltea=more_tea+tea_types
+print(alltea)
+# count method
+print(type(tea_types))
