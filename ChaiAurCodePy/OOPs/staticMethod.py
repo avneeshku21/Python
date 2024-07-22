ecf111cc621d6ea2c708089a743a51f15b07af4f
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-22 13:12:54 +0530

    update

--- ChaiAurCodePy/OOPs/staticMethod.py
+++ ChaiAurCodePy/OOPs/staticMethod.py
@@ -14,16 +14,19 @@ class Car:
     total_car=0
     def __init__(self,brand,model):
         self.brand=brand
-        self.model=model
+        self.__model=model
         Car.total_car+=1
     def full_name(self):
-        return f"{self.brand}{self.model}"
+        return f"{self.brand}{self.__model}"
     def fuel_type(self):
        return "Petrol or Diesel"
     
     @staticmethod 
     def general_des():
        return "Cars are means of transport"
+    @property
+    def model(self):
+       return self.__model
     
 class ElectricCar(Car):
     def __init__(self,brand,model,battery_size):
@@ -33,6 +36,9 @@ class ElectricCar(Car):
     def fuel_type(self):
        return "Electric Charge"
 
+my_tesla=ElectricCar("tesla","Model s","85kwh")
 myCar=Car("Mahendra","Thar")
+print(isinstance(my_tesla,Car))
+print(isinstance(my_tesla,ElectricCar))
+# print(myCar.model)
 #print(myCar.general_des())
-print(Car.general_des())
