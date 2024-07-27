704a053a5e1e14b7b75c5d2f2367d34d39a25293
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-27 18:53:27 +0530

    update

---
+++ Session4/List.py
@@ -0,0 +1,57 @@
+
+#****List is Dynamic
+#list me ham value store nhi krte...... hai ham uske address ko store krte hai
+#speed of execution is slow because  ham list double krte 
+# hai aur copy krte hai ( basically dynamic hai)
+
+
+
+# list=[1,2,3]
+# print(id(list))  # id  give addresss
+# print(id(list[0]))
+# print(id(list[1]))
+# print(id(list[2]))
+# print(id(1))
+# print(id(2))
+# print(id(3))
+
+# list[0]="a"
+# print(list[0])
+# print(id(list[0]))
+ #Here the memory address will be change after change value of list 
+
+# ***********characterstics of list
+# orderd
+# chanagable/mutale
+# Hetrogeneous
+# can have duplicates
+# are dyanamic
+# can be nested
+# items can be accessed
+# we   can add contain any kind of objects in python
+
+# L=[1,2,3]
+# L1=[3,2,1]
+# print(L==L1) # here order is not same
+#************************************************
+# print([])
+#1D
+# print([12,3,4,5,6])
+#2D
+# print([1,2,3,4,5,[4,5]])
+#3D
+
+
+#*******Hetrogenous
+# print([1,2,3,True,"Helo"])
+
+#*******Using Type Coversion
+#print(list('Hello')) #['H', 'e', 'l', 'l', 'o']
+
+
+
+
+
+
+
+
