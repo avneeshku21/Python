0a33922d6d1d52d60f752e8e6ce3d39a8735f910
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-31 19:17:28 +0530

    update

---
+++ Session4/Traverselist.py
@@ -0,0 +1,14 @@
+
+#***************2 ways to traverse a list
+# itemwise
+# indexwise
+
+#****itemwise
+# l=[1,2,3,4,5,6]
+# for i in l:
+#     print(i)
+
+# *****indexwise
+l=[1,2,3,4,5]
+for i in range(0,len(l)):
+    print(l)
