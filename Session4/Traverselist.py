47b208736ad604e9d2a8a6ca332c1a4a469a9a03
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-31 21:32:13 +0530

    update

--- Session4/Traverselist.py
+++ Session4/Traverselist.py
@@ -11,4 +11,10 @@
 # *****indexwise
 l=[1,2,3,4,5]
 for i in range(0,len(l)):
-    print(l)
+    print(i,l[i])
+
+
+# Zip
+# The zip() function returns a zip object, which is an iterator of tuples where the first item in each passed iterator is paired together, and then the second item in each passed iterator are paired together.
+
+# If the passed iterators have different lengths, the iterator with the least items decides the length of the new iterator.
