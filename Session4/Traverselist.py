11a2a9ff39cff7aead30dac4b80329baa6c60e27
Avneesh Kumar <kaviikumar010@gmail.com>
2024-08-01 10:36:18 +0530

    update

--- Session4/Traverselist.py
+++ Session4/Traverselist.py
@@ -3,18 +3,36 @@
 # itemwise
 # indexwise
 
+
 #****itemwise
 # l=[1,2,3,4,5,6]
 # for i in l:
 #     print(i)
 
+
 # *****indexwise
-l=[1,2,3,4,5]
-for i in range(0,len(l)):
-    print(i,l[i])
+# l=[1,2,3,4,5]
+# for i in range(0,len(l)):
+#     print(i,l[i])
 
 
 # Zip
 # The zip() function returns a zip object, which is an iterator of tuples where the first item in each passed iterator is paired together, and then the second item in each passed iterator are paired together.
 
 # If the passed iterators have different lengths, the iterator with the least items decides the length of the new iterator.
+
+# L1 = [1,2,3,4]
+# L2 = [-1,-2,-3,-4]
+
+# list(zip(L1,L2)) 
+
+# [i+j for i,j in zip(L1,L2)]
+
+# L = [1,2,print,type,input] #list me ham builtin function bhi store kr skte hai
+
+# print(L)
+
+# Disadvantages of Python Lists
+# Slow
+# Risky usage
+# eats up more memory
