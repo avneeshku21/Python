f4e4fb6626f4d305ad8de850a6e8790fa9cef7c8
Avneesh Kumar <kaviikumar010@gmail.com>
2024-08-03 13:27:56 +0530

    update

---
+++ OOPS-1/MethonandFunc.py
@@ -0,0 +1,7 @@
+# What is Difference between method and Function
+# Ans - jab koi function class k ander hai bo method hai
+
+# aur jo function outside tha class hai bo function
+
+# Note  + means publice
+#       - means private
