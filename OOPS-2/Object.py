b8e571fcae80288ac4676746930b4536fc0d5962
Avneesh Kumar <kaviikumar010@gmail.com>
2024-08-04 13:58:13 +0530

    update

---
+++ OOPS-2/Object.py
@@ -0,0 +1,17 @@
+
+#* Collection of objects
+
+# list of objects
+class Person:
+
+  def __init__(self,name,gender):
+    self.name = name
+    self.gender = gender
+
+p1 = Person('Avneesh','male')
+p2 = Person('Priya','Female')
+p3 = Person('Khushi','female')
+
+# l=[p1,p2,p3]
+# for i in l:
+#   print(i.name,i.gender)
---
+++ OOPS-2/static.py
@@ -0,0 +1,12 @@
+# Static Variables(Vs Instance variables)
+
+# In Python, a static variable is a variable that is shared among all instances of a class, rather than being unique to each instance. It is also sometimes referred to as a class variable, because it belongs to the class itself rather than any particular instance of the class.
+
+# static vs instance
+# instance -  instance varibale ka value har object k liye alg alg  hota hai
+
+# instance varibale object ka hota hai
+
+# static - static varibale ka value har object k liye same hota hai
+
+# static varible class ka hota hai
