994bb2f7cfabb36459ec62831a1c9b42333a5bd2
Avneesh Kumar <kaviikumar010@gmail.com>
2024-08-06 13:44:12 +0530

    update

--- OOPS-3/TypeInheritance.py
+++ OOPS-3/TypeInheritance.py
@@ -99,3 +99,50 @@ s=SmartPhone(20000, "Apple", 12)
 
 s.buy()
 s.review()
+
+
+
+# the diamond problem
+# https://stackoverflow.com/questions/56361048/what-is-the-diamond-problem-in-python-and-why-its-not-appear-in-python2
+# class Phone:
+#     def __init__(self, price, brand, camera):
+#         print ("Inside phone constructor")
+#         self.__price = price
+#         self.brand = brand
+#         self.camera = camera
+
+#     def buy(self):
+#         print ("Buying a phone")
+
+# class Product:
+#     def buy(self):
+#         print ("Product buy method")
+
+# # Method resolution order
+# class SmartPhone(Phone,Product):
+#     pass
+
+# s=SmartPhone(20000, "Apple", 12)
+
+# s.buy()
+
+# Polymorphism
+
+# Having multiple face mtlb sitution k according behave kre
+
+
+# *Method Overriding
+# Inheritance me  parent or child k pass same method ho   tab child class ka method call hota hai
+
+# *Method Overloading
+
+# single class m ek name ke do method hai aur dono ka behaviour alg alg hai depanding on the input
+
+# benifit
+#  Code readablity
+
+# *Operator Overloading
+# sme operatore depend on input to bheaviour alg alg hojata hai
+
+"Hello"+"World" # conactionation
+[1,2,3,4]+[6,7,8,9] # merging
